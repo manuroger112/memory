1a6f141bee724903c0ddaf25bab52dc1ccc9c432
manuroger112 <135890182+manuroger112@users.noreply.github.com>
2023-08-20 16:39:32 +0200

    Update memory.h

--- memory.h
+++ memory.h
@@ -1,94 +1,111 @@
 #pragma once
 #include <windows.h>
 #include <TlHelp32.h>
-
 #define WIN32_LEAN_AND_MEAN
 
 
 
-namespace VARS {
 
+namespace VARS {
 	DWORD GetProcess(const wchar_t* Target) {
-		DWORD procId = 0;
-		HANDLE snapShotHandle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-		PROCESSENTRY32W processEntry = {};
+		HANDLE snapshotHandle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+		if (snapshotHandle == INVALID_HANDLE_VALUE) {
+			return NULL;
+		}
+
+		PROCESSENTRY32W processEntry = { };
 		processEntry.dwSize = sizeof(PROCESSENTRY32W);
 
-		if (Process32FirstW(snapShotHandle, &processEntry)) {
-
+		if (Process32FirstW(snapshotHandle, &processEntry)) {
 
 			do {
 
 				if (_wcsicmp(processEntry.szExeFile, Target) == 0) {
-					procId = processEntry.th32ProcessID;
-
-					break;
+					CloseHandle(snapshotHandle);
+					return processEntry.th32ProcessID;
 				}
-			} while (Process32NextW(snapShotHandle, &processEntry));
+
+
+
+			} while (Process32NextW(snapshotHandle, &processEntry));
+
+
+
 		}
-		//VARS::processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, VARS::processId);
-		CloseHandle(snapShotHandle);
-		return procId;
+
+		CloseHandle(snapshotHandle);
+		return NULL;
+
 	}
 
 
 
+	uintptr_t GetModuleBaseAddress(DWORD processId, const wchar_t* ModuleTarget) {
 
+		HANDLE snapshotHandle = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, processId);
 
-
-	uintptr_t GetModuleBaseAddress(DWORD processId, const wchar_t* moduleName) {
-		uintptr_t modBaseAddr = 0;
-		HANDLE snapshotModule = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, processId);
-		if (snapshotModule == INVALID_HANDLE_VALUE) {
-			return 0;
+		if (snapshotHandle == INVALID_HANDLE_VALUE) {
+			return NULL;
 		}
 
-		MODULEENTRY32W moduleEntry;
+		MODULEENTRY32W moduleEntry = { };
 		moduleEntry.dwSize = sizeof(MODULEENTRY32W);
 
-		if (Module32FirstW(snapshotModule, &moduleEntry)) {
-			do {
-				if (_wcsicmp(moduleEntry.szModule, moduleName) == 0) {
-					modBaseAddr = reinterpret_cast<uintptr_t>(moduleEntry.modBaseAddr);
-					CloseHandle(snapshotModule);
+		if (Module32FirstW(snapshotHandle, &moduleEntry)) {
 
+			do {
+
+				if (_wcsicmp(moduleEntry.szModule, ModuleTarget) == 0) {
+					CloseHandle(snapshotHandle);
+					return reinterpret_cast<uintptr_t>(moduleEntry.modBaseAddr);
 				}
-			} while (Module32NextW(snapshotModule, &moduleEntry));
+
+
+
+			} while (Module32NextW(snapshotHandle, &moduleEntry));
+
+
+
 		}
 
+		CloseHandle(snapshotHandle);
+		return NULL;
 
-
-		CloseHandle(snapshotModule);
-		return modBaseAddr;
 	}
 
 
 
-	
-	HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, NULL, GetProcess(L"csgo.exe"));;
-	uintptr_t baseAddress = GetModuleBaseAddress(GetProcess(L"csgo.exe"), L"client.dll");
-	uintptr_t engineAddress = GetModuleBaseAddress(GetProcess(L"csgo.exe"), L"engine.dll");
-	HDC hdc = GetDC(FindWindowA(NULL, "Counter-Strike: Global Offensive"));
+
+
+	DWORD processId = GetProcess(L"csgo.exe");
+
+	uintptr_t baseAddress = GetModuleBaseAddress(processId, L"client.dll");
+
+
+	HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
 
 
 
+	template<typename type>
 
-	template <typename type>
 	type memRead(uintptr_t pointerStatic) {
+
 		type value = { };
-		ReadProcessMemory(VARS::processHandle, (LPVOID)pointerStatic, &value, sizeof(type), NULL);
+
+		ReadProcessMemory(processHandle, (LPCVOID)pointerStatic, sizeof(type), NULL);
+
 		return value;
+
 	}
 
-	template <typename type>
+
+	template<typename type>
+
 	bool memWrite(uintptr_t pointerStatic, type value) {
 
-		return WriteProcessMemory(VARS::processHandle, (LPVOID)pointerStatic, &value, sizeof(type), NULL);
+		return WriteProcessMemory(processHandle, (LPVOID)pointerStatic, value, sizeof(type), NULL);
 
 	}
 
-
-
-	
-	
 }
